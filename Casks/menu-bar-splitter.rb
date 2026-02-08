5b0ee4797911165e29f4f60b29cae086c74fd420
Mohamad Yahia <xyzt70@gmail.com>
2026-02-09 03:13:32 +0400

    Update SHA for notarized build

--- Casks/menu-bar-splitter.rb
+++ Casks/menu-bar-splitter.rb
@@ -1,6 +1,6 @@
 cask "menu-bar-splitter" do
   version "2.0.1"
-  sha256 "7adb54e30c0d5d6feb18b146cd331e9e08740d71d8e7c223207f2b3d2955f151"
+  sha256 "fae93f11a6ff12b2f17b38d22a80dafa31435b7e8a012c0c77c333e7b2b7f6e3"
 
   url "https://github.com/maddada/menu-bar-splitter/releases/download/v#{version}/MenuBarSplitter.zip"
   name "Menu Bar Splitter"
