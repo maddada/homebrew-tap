077668315726e973039318c15aab1f4ae22400a9
Mohamad Yahia <xyzt70@gmail.com>
2026-02-09 03:21:16 +0400

    Update menu-bar-splitter to v2.1

--- Casks/menu-bar-splitter.rb
+++ Casks/menu-bar-splitter.rb
@@ -1,6 +1,6 @@
 cask "menu-bar-splitter" do
-  version "2.0.1"
-  sha256 "fae93f11a6ff12b2f17b38d22a80dafa31435b7e8a012c0c77c333e7b2b7f6e3"
+  version "2.1"
+  sha256 "368dd2c679ce2193c566241ceed7be48ed579e5fc0776312b0a70c9e2fa2b332"
 
   url "https://github.com/maddada/menu-bar-splitter/releases/download/v#{version}/MenuBarSplitter.zip"
   name "Menu Bar Splitter"
