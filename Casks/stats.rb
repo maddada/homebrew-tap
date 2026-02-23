ee2f166f870dcb074216a2b2228c735b5f16af85
Mohamad Yahia <xyzt70@gmail.com>
2026-02-23 13:47:09 +0400

    update stats cask to v3.0.0 with DMG and new bundle ID

--- Casks/stats.rb
+++ Casks/stats.rb
@@ -1,11 +1,18 @@
 cask "stats" do
-  version "2.11.66"
-  sha256 "9d820055c8f692bc01160d2a80b95c87968c535b2503b3a5d43d7a96df68fb5e"
+  version "3.0.0"
+  sha256 "c94c906c45f79465a8d46bdae05ab9d4573d322b43ffac8067ff1700bda1f63b"
 
-  url "https://github.com/maddada/stats/releases/download/v#{version}/Stats.zip"
+  url "https://github.com/maddada/stats/releases/download/v#{version}/Stats.dmg"
   name "Stats"
   desc "macOS system monitor in your menu bar"
   homepage "https://github.com/maddada/stats"
 
+  depends_on macos: ">= :big_sur"
+
   app "Stats.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.madda.Stats.plist",
+    "~/Library/Saved Application State/com.madda.Stats.savedState",
+  ]
 end
