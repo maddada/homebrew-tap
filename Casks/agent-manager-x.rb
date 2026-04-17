dae973b1ed59d4cc12d504eae5b48cb94c7d1ca0
Mohamad Yahia <xyzt70@gmail.com>
2026-04-17 04:10:44 +0400

    chore(cask): bump agent-manager-x to v2.2

--- Casks/agent-manager-x.rb
+++ Casks/agent-manager-x.rb
@@ -1,13 +1,13 @@
 cask "agent-manager-x" do
-  version "2.1"
+  version "2.2"
 
   on_arm do
-    sha256 "346b304a2bbfffe2322905280b5c1ee7f43495afaa927400cbdcbc0888720a3a"
+    sha256 "a4adee62911afee978a7affec803ff3102040a78bc86804d3693c3c231356b3f"
     url "https://github.com/maddada/agent-manager-x/releases/download/v#{version}/AgentManagerX_#{version}_aarch64.dmg"
   end
 
   on_intel do
-    sha256 "77a829ae62ea56bdc118aa7d5e25e688f3741b0f5f5cf06a5761311ef3b33e38"
+    sha256 "383b1f6466a2f4758e55ea8d7bed678ce115e46d6e57101df86b3d2374dffd5b"
     url "https://github.com/maddada/agent-manager-x/releases/download/v#{version}/AgentManagerX_#{version}_x64.dmg"
   end
 
