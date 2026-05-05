80110c51b6590998523de1471c4014d49cc97105
Mohamad Yahia <xyzt70@gmail.com>
2026-05-06 03:24:15 +0400

    Update zmux cask to 1.4.7

--- Casks/zmux.rb
+++ Casks/zmux.rb
@@ -1,6 +1,6 @@
 cask "zmux" do
-  version "1.4.6"
-  sha256 "8896ce0f154f376933013f7d47c635fc1a1507ea351b28396df56368635fd430"
+  version "1.4.7"
+  sha256 "365054d2977a8736b2af138e7d68e4e2d9fe6aa49b8556b8833df29d30e6bde5"
 
   url "https://github.com/maddada/zmux/releases/download/v#{version}/zmux-#{version}.dmg"
   name "zmux"
