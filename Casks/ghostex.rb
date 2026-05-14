27d4657ac724f195fd653ba6d401646f5522a9ad
Mohamad Yahia <xyzt70@gmail.com>
2026-05-14 11:13:37 +0400

    Add Ghostex cask 2.5.0

---
+++ Casks/ghostex.rb
@@ -0,0 +1,22 @@
+cask "ghostex" do
+  version "2.5.0"
+  sha256 "ebbf925a8765990b5f6b18b466a6f7e824dfc88b1dbb9046fbbd72d97a7d1c06"
+
+  url "https://github.com/maddada/Ghostex/releases/download/v2.5/ghostex-#{version}-arm64.dmg"
+  name "Ghostex"
+  desc "Workspace and session UI for agent terminals"
+  homepage "https://github.com/maddada/Ghostex"
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :ventura"
+
+  conflicts_with cask: "zmux"
+
+  app "Ghostex.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.madda.zmux.host",
+    "~/Library/Preferences/com.madda.zmux.host.plist",
+    "~/Library/Saved Application State/com.madda.zmux.host.savedState",
+  ]
+end
