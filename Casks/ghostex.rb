6f930f1c91b332fa5b5eace64a2a1162ffa10985
Mohamad Yahia <xyzt70@gmail.com>
2026-05-23 02:48:42 +0400

    Update ghostex cask to 3.9.1

--- Casks/ghostex.rb
+++ Casks/ghostex.rb
@@ -1,9 +1,9 @@
 cask "ghostex" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "3.9.0"
-  sha256 arm:   "13f5423c9de354bc93d6ae2c865d2d2b25e86c843558b54e90ee3978c192bdac",
-         intel: "ddf69a3157d3b8b1e6ebd1f71a79ae614755b008816d3240be19f199779b2d57"
+  version "3.9.1"
+  sha256 arm:   "f551f0bbe68ea479d57e726515510707d4757470933b09cdc512e3b06d85e599",
+         intel: "13581e31c7020c91c22f891293e2776db76a7889d7bf9b517b1cb2404aeca3ee"
 
   url "https://github.com/maddada/Ghostex/releases/download/v#{version}/ghostex-#{version}-#{arch}.dmg"
   name "Ghostex"
